83ebd1e27f2ef14b19dc8eece831df82bdecbecd
JOSEPH QUESTED <josephquested@gmail.com>
2020-08-14 00:58:42 +1200

    decay working

--- editData.js
+++ editData.js
@@ -8,6 +8,7 @@ const increaseIKE = (amount, cb) => {
     ikeTotal += amount
     data.ikeTotal = ikeTotal
     data.day++
+    decayStudents(data.students);
     writeData(data, cb)
 }
 
@@ -18,13 +19,33 @@ const decreaseIKE = (amount, cb) => {
 
 const reset = (cb) => {
     data.students.forEach((student, i) => {
-        student = backup.students[i]
+        student.hunger = backup.students[i].hunger
+        student.thirst = backup.students[i].thirst
+        student.fatigue = backup.students[i].fatigue
+        student.whelm = backup.students[i].whelm
     })
 
     data.ikeTotal = backup.ikeTotal
     data.day = backup.day
 
     writeData(data, cb)
+}
+
+
+// -- UTIL -- //
+
+const decayStudents = (students) => {
+    students.forEach((student) => {
+        student.hunger -= random()
+        student.thirst -= random()
+        student.fatigue -= random()
+        student.whelm -= random()
+    })
+}
+
+const random = () => {
+    const difficulty = 3;
+    return Math.floor(Math.random() * difficulty) 
 }
 
 module.exports = { increaseIKE, decreaseIKE, reset }
