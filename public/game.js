cf35eceb5f29122f7ed0b9301b48eb231b088698
JOSEPH QUESTED <josephquested@gmail.com>
2020-08-13 18:23:02 +1200

    can increase ike mined

--- public/game.js
+++ public/game.js
@@ -1,15 +1,19 @@
 // -- INIT -- //
 
-const ikeButton = document.getElementById("ikeButton");
+const ikeButton = document.getElementById('ikeButton')
+const ikeMined = document.getElementById('ikeMined')
 
 function bindEventListeners () {
-    ikeButton.addEventListener("click", ikeButtonClicked);
+    ikeButton.addEventListener('click', ikeButtonClicked);
 }
 
 bindEventListeners();
 
 // -- IKE BUTTON -- //
 
+let _ikeMined = 0
+
 function ikeButtonClicked () {
-    console.log('clicked')
+    _ikeMined++
+    ikeMined.innerHTML = `I.K.E MINED: ${_ikeMined}`
 }
