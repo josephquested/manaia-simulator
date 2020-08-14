e89d0ffc675202e83aa4bcbe509e293512382832
JOSEPH QUESTED <josephquested@gmail.com>
2020-08-14 14:05:05 +1200

    add tests

---
+++ tests/index.test.js
@@ -0,0 +1,47 @@
+test('JV IS SO PROUD OF ME', () => {
+    expect(true).toBeTruthy()
+  })
+  
+test('JV IS MY BEST FRIEND', () => {
+    expect(true).toBeTruthy()
+  })
+
+test('JV LOVES TO TEST', () => {
+    expect(true).toBeTruthy()
+  })
+
+test('JV IS MY DADDY', () => {
+    expect(true).toBeTruthy()
+  })
+
+test('JV IS THE GOD KIND', () => {
+    expect(true).toBeTruthy()
+  })
+
+test('JV CAN DO NO WRONG', () => {
+    expect(true).toBeTruthy()
+  })
+
+test('JV IS MY REAL FATHER', () => {
+    expect(true).toBeTruthy()
+  })
+
+test('JV FOR PRESIDENT', () => {
+    expect(true).toBeTruthy()
+  })
+
+test('JV IS AN ALL AUSTRAILIAN HERO', () => {
+    expect(true).toBeTruthy()
+  })
+
+test('JV WRITES EXCELLENT TESTS', () => {
+    expect(true).toBeTruthy()
+  })
+
+test('JV COULDNT BE PROUDER OF ME', () => {
+    expect(true).toBeTruthy()
+  })
+
+test('JV SOARS LIKE AN EAGLE', () => {
+    expect(true).toBeTruthy()
+  })
